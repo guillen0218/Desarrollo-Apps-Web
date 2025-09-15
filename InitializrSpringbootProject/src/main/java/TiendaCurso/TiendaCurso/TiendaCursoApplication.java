9dd2027d2bcc5db6518b80f032f4c5a7923a4c59
jguil <jguil@DESKTOP-T4LP35R>
2025-09-15 12:04:30 -0600

    Ini Proyecto

---
+++ InitializrSpringbootProject/src/main/java/TiendaCurso/TiendaCurso/TiendaCursoApplication.java
@@ -0,0 +1,13 @@
+package TiendaCurso.TiendaCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaCursoApplication.class, args);
+	}
+
+}
